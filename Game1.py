8e00618b030dff19d5f36b440dc0a13e70878fc4
Sayandeep Das <128297747+sayandeep2921@users.noreply.github.com>
2023-08-15 21:37:37 +0530

    Add files via upload
    Guess a number between 1 to 100

---
+++ Game1.py
@@ -0,0 +1,28 @@
+import random
+randNumber = random.randint(1,100)
+
+userGuess = None
+guesses = 0
+
+while(userGuess != randNumber):
+    userGuess = int(input("enter your guess:"))
+    guesses += 1
+    if(userGuess==randNumber):
+        print("you guessed it right")
+    else:
+        if (userGuess>randNumber):
+            print ("you guessed it wrong , enter a smaller number")
+        else:
+            print("you guessed it wrong !, enter a larger number ")
+    
+
+
+print(f"you guessed the number in {guesses} guesses")
+
+with open("hiscore.txt","r") as f:
+    hiscore = int(f.read())
+
+if(guesses<hiscore):
+    print("you have just broken the high score!")
+    with open("hiscore.txt","w") as f:
+        f.write(guesses)
